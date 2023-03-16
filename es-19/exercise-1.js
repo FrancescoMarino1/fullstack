57fc3016e3ba22d4bf423b4c5a7b9a332165366e
francesco <marinofrancesco184@gmail.com>
2023-03-16 17:41:01 +0100

    complete

--- es-19/exercise-1.js
+++ es-19/exercise-1.js
@@ -8,6 +8,11 @@ const person2 = person1;
 
 // Modify the property `firstName` of the `person2` in "Simon".
 
+person2.firstName = "Simon";
+
+
 console.log(person1);
 console.log(person2);
-ytdyirdx
+// a causa di p2=p1 non si puo modificare il nome solo del person2 dato che non si va a creare un nuovo oggetto ma si va a modificare quello 
+// gia esistente 
+
